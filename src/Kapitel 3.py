c81ea70376bdae84c910232df983917f469ec371
Maximilian Winkler <maximilian.winkler01@stud.uni-goettingen.de>
2020-11-29 11:58:17 +0100

    straight_line_point für einzelne Punkte auf einer Geraden, straight_line_function gibt eine Geradenfunktion zurück, ratio "ohne projektion auf achse"

--- src/Kapitel 3.py
+++ src/Kapitel 3.py
@@ -1,30 +1,32 @@
 import numpy as np
-from scipy.spatial import distance
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from typing import Tuple, Callable
+import functools
 
 
-def straight_line(a: np.ndarray, b: np.ndarray, t: float = 0.5, phi: Callable[[np.ndarray], np.ndarray] = lambda x: x) \
-        -> np.ndarray:
-    return (1 - t) * phi(a) + t * phi(b)
+def straight_line_point(a: np.ndarray, b: np.ndarray, t: float = 0.5) -> np.ndarray:
+    return (1 - t) * a + t * b
+
+
+def straight_line_function(a, b) -> functools.partial:
+    return functools.partial(straight_line_point, a, b)
 
 
 def ratio(left: np.ndarray, col_point: np.ndarray, right: np.ndarray) -> float:
-    d1 = (col_point - left).reshape(-1, 1)
-    d2 = (right - col_point).reshape(-1, 1)
-    print(d1)
-    print(d2)
-    return (d1 / d2)[0][0]
+    for i in range(len(left)):
+        if left[i] == right[i]:
+            continue
+        return (col_point[i] - left[i]) / (right[i] - col_point[i])
+    return 0
 
 
 def ratio_test() -> None:
-    p1 = np.array([0, 0, 0])
-    p2 = np.array([1, 1, 1])
-    p3 = np.array([.66, .66, .66])
-    test = ratio(p1, p3, p2)
+    left = np.array([0, 0, 0])
+    right = np.array([1, 1, 1])
+    col_point = np.array([.66, .66, .66])
+    test = ratio(left, col_point, right)
     print(test)
-    print(type(test))
 
 
 def straight_line_test() -> None:
@@ -32,7 +34,7 @@ def straight_line_test() -> None:
     fig = plt.figure()
     ax = Axes3D(fig)
     while t <= 1:
-        test = straight_line(np.array([0, 0, 0]), np.array([1, 1, 1]), t)
+        test = straight_line_point(np.array([0, 0, 0]), np.array([1, 1, 1]), t)
         ax.scatter(test[0], test[1], test[2])
         t += 0.1
     plt.show()
@@ -48,23 +50,3 @@ if __name__ == "__main__":
     init()
 
 #####################################################################################################
-"""
-def straight_line(a: tuple, b: tuple, scale: float=0) -> Tuple[list, list, list]:
-    t = -abs(scale)
-    xs = []
-    ys = []
-    zs = []
-    while t <= abs(scale):
-        xs.append((1-t)*a[0]+t*b[0])
-        ys.append((1-t)*a[1]+t*b[1])
-        if len(a) == 3 and len(b) == 3:
-            zs.append((1-t)*a[2]+t*b[2])
-        t += 0.01
-    return xs, ys, zs
-
-"""
-
-""" ????
-def ratio(a: tuple, b: tuple, c: tuple) -> float:
-    return abs(distance.euclidean(a, b)/distance.euclidean(b, c))
-"""
