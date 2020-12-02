0be936632e35df186bf6c5d1e5426ac2708f4180
Maximilian Winkler <maximilian.winkler01@stud.uni-goettingen.de>
2020-12-02 15:57:00 +0100

    numpy doc for straight_line_point, straight_line_function and ratio

--- src/Kapitel 3.py
+++ src/Kapitel 3.py
@@ -6,14 +6,60 @@ import functools
 
 
 def straight_line_point(a: np.ndarray, b: np.ndarray, t: float = 0.5) -> np.ndarray:
+    """
+    method to calculate a single point on a straight line through a and b
+
+    Parameters
+    ----------
+    a: np.ndArray
+        first point on straight line to calculate new point
+    b: np.ndArray
+        second point on straight line to calculate new point
+    t: float
+        for the weight of a and b
+
+    Returns
+    -------
+    new point on straight line through a and b
+    """
     return (1 - t) * a + t * b
 
 
-def straight_line_function(a, b) -> functools.partial:
+def straight_line_function(a: np.ndarray, b: np.ndarray) -> functools.partial:
+    """
+    method to get the function of a straight line through a and b
+
+    Parameters
+    ----------
+    a: np.ndArray
+        first point on straight line
+    b: np.ndArray
+        second point on straight line
+
+    Returns
+    -------
+    function for the straight line through a and b
+    """
     return functools.partial(straight_line_point, a, b)
 
 
 def ratio(left: np.ndarray, col_point: np.ndarray, right: np.ndarray) -> float:
+    """
+    method to calculate the ratio of the three collinear points from the parameters
+
+    Parameters
+    ----------
+    left: np.ndArray
+        left point that defines the straight line
+    col_point: np.ndArray
+        collinear point to left and right, could be the most left or most right point or between left and right
+    right: np.ndArray
+        right point that defines the straight line
+
+    Returns
+    -------
+    the ratio of the three collinear points from the parameters
+    """
     for i in range(len(left)):
         if left[i] == right[i]:
             continue
@@ -29,7 +75,7 @@ def ratio_test() -> None:
     print(test)
 
 
-def straight_line_test() -> None:
+def straight_line_point_test() -> None:
     t = 0
     fig = plt.figure()
     ax = Axes3D(fig)
@@ -41,7 +87,7 @@ def straight_line_test() -> None:
 
 
 def init() -> None:
-    # straight_line_test()
+    straight_line_point_test()
     ratio_test()
     # print(ratio((0, 0), (1, 1), (10, 10)))
 
