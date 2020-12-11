7e57be81adeaf438c99196b230617ba96165e9a9
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2020-12-11 09:48:32 +0100

    First Implementation DeCasteljau

--- src/Kapitel5.py
+++ src/Kapitel5.py
@@ -5,6 +5,7 @@ from scipy.special import comb
 from abc import ABC, abstractmethod
 from typing import Optional
 
+
 # Ways to compute bezier curve
 # Blossoming / No Blossoming
 # Keeping Intermediates / Not keeping
@@ -55,9 +56,11 @@ class AbstractBezierCurve(ABC):
         self._calc_value(self._affine_parameter_transformation_helper(t))
 
     def __eq__(self, other):
-        # Needed for cache lookup
-        # Why we prefer isinstance:
-        # https://stackoverflow.com/a/1549854/9958281
+        """
+        This equality is function equality (post parameter transformation),
+        not approximation equality, since we do not check the
+        approximation accuracy.
+        """
         if isinstance(other, AbstractBezierCurve):
             return self.n == other.n and self.k == other.k and self.bs
 
@@ -70,9 +73,16 @@ class AbstractBezierCurve(ABC):
             self._compute_interval_values()
         return self.computed_interval_values
 
-    @abstractmethod
     def _compute_interval_values(self):
-        ...
+        """
+        This is okay to be overwritten. Most of the times, we want to compute the values by
+        calculating enough evenly spaced values.
+        If this is not the wanted behaviour (when for example the function should be approximated by
+        other functions) this function should be overwritten.
+        """
+        a, b = self.interval_closed
+        steps_arr = np.linspace(a, b, self.steps)
+        return [self._calc_value(t) for t in steps_arr]
 
     # Begin helper methods
     def _uses_default_interval(self):
@@ -87,25 +97,26 @@ class BezierCurveBernsteinBasis(AbstractBezierCurve):
     def _calc_value(self, t):
         return sum([self.bs[self.k + i] * bernstein(self.n, i, t) for i in range(self.n + 1)])
 
-    def _compute_interval_values(self):
-        a, b = self.interval_closed
-        steps_arr = np.linspace(a, b, self.steps)
-        return [self._calc_value(t) for t in steps_arr]
 
-
+# TODO: implement derivative and stuff
 class BezierCurveDeCasteljau(AbstractBezierCurve):
-    _BEZIER_CURVE_CACHE = []
 
-    def __new__(cls, bs, n, k: Optional[int] = None, interval_closed=(0, 1), steps=1000):
-        ...
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.de_casteljau_scheme = None  # TODO: Check if this is needed
+
+        self._generate_de_casteljau_scheme()
+
+    def _generate_de_casteljau_scheme(self):
+        base_case = [(lambda t: b) for b in self.bs]
+        self.de_casteljau_scheme = [base_case]
+        for n in range(1, self.n + 1):
+            self.de_casteljau_scheme.append(
+                [self._generate_single_curve(n, k) for k in range(len(self.de_casteljau_scheme[n - 1]) - 1)]
+            )
+
+    def _generate_single_curve(self, n, k):
+        return lambda t: (1 - t) * self.de_casteljau_scheme[n - 1][k] + self.de_casteljau_scheme[n - 1][k + 1](t)
 
     def _calc_value(self, t):
-        ...
-
-    def _compute_interval_values(self):
-        ...
-
-    # Begin helper methods
-    @classmethod
-    def _get_cache(cls, n, k):
-        ...
+        return self.de_casteljau_scheme[-1][0](t)
