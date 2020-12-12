0b0c538a1d52f7a499ea1e8fe32bd8e4d6b18242
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2020-12-12 23:28:09 +0100

    bezier_curve.py only contains bezier_curve related stuff

--- src/bezier_curve.py
+++ src/bezier_curve.py
@@ -5,6 +5,8 @@ import threading as th
 import shapely.geometry as sg
 from typing import Tuple, Callable
 
+from src.utilities import csv_read
+
 lockMe = th.Lock()  # variable used to control access of threads
 
 
@@ -523,28 +525,6 @@ class BezierCurve3D(BezierCurve2D):
         ax = plt.axes(projection='3d')
         ax.scatter3D(xs, ys, zs)
         plt.show()
-
-
-def csv_read(file_path: str) -> np.ndarray:
-    try:
-        with open(file_path, 'r') as csv_file:
-            xs, ys, zs = [], [], []
-            for line in csv_file:
-                try:
-                    x, y, z = line.split(',')
-                    zs.append(float(z))
-                except ValueError:
-                    try:
-                        x, y = line.split(',')
-                    except ValueError:
-                        print('Expected two or three values per line')
-                        return np.array([])
-                xs.append(float(x))
-                ys.append(float(y))
-        return np.array([xs, ys], dtype=float) if not zs else np.array([xs, ys, zs], dtype=float)
-    except FileNotFoundError:
-        print(f'File: {file_path} does not exist.')
-        return np.array([])
 
 
 def init() -> None:
---
+++ src/utilities.py
@@ -0,0 +1,20 @@
+def csv_read(file_path: str) -> np.ndarray:
+    try:
+        with open(file_path, 'r') as csv_file:
+            xs, ys, zs = [], [], []
+            for line in csv_file:
+                try:
+                    x, y, z = line.split(',')
+                    zs.append(float(z))
+                except ValueError:
+                    try:
+                        x, y = line.split(',')
+                    except ValueError:
+                        print('Expected two or three values per line')
+                        return np.array([])
+                xs.append(float(x))
+                ys.append(float(y))
+        return np.array([xs, ys], dtype=float) if not zs else np.array([xs, ys, zs], dtype=float)
+    except FileNotFoundError:
+        print(f'File: {file_path} does not exist.')
+        return np.array([])
