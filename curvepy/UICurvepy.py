3cd0d514ee43670ade025b017a04f181b3caaa2c
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2020-12-19 21:21:22 +0100

    is it just mpl?

--- curvepy/UICurvepy.py
+++ curvepy/UICurvepy.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QTabWidget, QVBoxLayout, \
     QGridLayout, QSlider, QGroupBox, QLabel
