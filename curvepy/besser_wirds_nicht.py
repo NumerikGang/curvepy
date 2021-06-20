c2943d7ae1d9421768678c1ec511f799a5d351d4
Maximilian Winkler <maximilian.winkler01@stud.uni-goettingen.de>
2021-06-20 15:38:36 +0200

    vor performanterer Nachbarerstellung

--- curvepy/besser_wirds_nicht.py
+++ curvepy/besser_wirds_nicht.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random as rd
 from functools import cached_property
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Set, Dict
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 
@@ -35,6 +35,7 @@ class Triangle:
 
     @property
     def points(self) -> List[Point2D]:
+        # If it was mutable caching would break
         return self._points
 
     @cached_property
@@ -53,6 +54,11 @@ class Triangle:
         A, B, C = self.points
         [x1, y1], [x2, y2], [x3, y3] = A, B, C
         d = 2 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+        # xu = (x1 * x1 + y1 * y1) / d * ((y2 - y3) + (x2 * x2 + y2 * y2)) / d * ((y3 - y1) + (x3 * x3 + y3 * y3)) / d * (
+        # y1 - y2) / d
+        # yu = (x1 * x1 + y1 * y1) / d * ((x3 - x2) + (x2 * x2 + y2 * y2)) / d * ((x1 - x3) + (x3 * x3 + y3 * y3)) / d * (
+        # x2 - x1) / d
+
         xu = ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)) / d
         yu = ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)) / d
 
@@ -86,7 +92,8 @@ class Triangle:
 
 class DelaunayTriangulation2D:
     def __init__(self, center: Point2D = (0, 0), radius: float = 1000):
-        self.supertriangles: List[Triangle] = self._create_supertriangles(center, radius)
+        self._neighbours = self._create_supertriangles(center, radius)
+        self.supertriangles: List[Triangle] = [*self.neighbours.keys()]
         self._triangles: List[Triangle] = [*self.supertriangles]
 
     @property
@@ -95,6 +102,10 @@ class DelaunayTriangulation2D:
         all_points_in_supertriangle = sum([x.points for x in self.supertriangles], [])
         remove_if = lambda t: any(pt in t.points for pt in all_points_in_supertriangle)
         return [t for t in self._triangles if not remove_if(t)]
+
+    @property
+    def neighbours(self):
+        raise NotImplementedError()
 
     def _create_supertriangles(self, center: Point2D, radius):
         # Since we have to start with a valid triangulation, we split our allowed range into 2 triangles like that:
@@ -122,7 +133,8 @@ class DelaunayTriangulation2D:
         lower_left, lower_right, upper_right, upper_left = [tuple(x) for x in base_rectangle]
         lower_triangle = Triangle(lower_left, lower_right, upper_left)
         upper_triangle = Triangle(upper_right, upper_left, lower_right)
-        return [lower_triangle, upper_triangle]
+        neighbours = {lower_triangle: {upper_triangle}, upper_triangle: {lower_triangle}}
+        return neighbours
 
     def add_point(self, p: Point2D):
         bad_triangles = [tri for tri in self._triangles if p in tri.circumcircle]
@@ -140,12 +152,12 @@ class DelaunayTriangulation2D:
     @staticmethod
     def _find_edges_only_used_by_a_single_triangle(triangles, no_duplicates=True):
         ret = []
-        for triangle in triangles:
+        for t in triangles:
             others = list(triangles)
-            others.remove(triangle)
-            for edge in triangle.edges:
-                if all(edge not in other.edges for other in others):
-                    ret.append(edge)
+            others.remove(t)
+            for e in t.edges:
+                if all(e not in o.edges for o in others):
+                    ret.append(e)
 
         # set() works since all edges are ordered, we don't care about the order and tuples are hashable
         if no_duplicates:
@@ -153,37 +165,71 @@ class DelaunayTriangulation2D:
 
         return ret
 
+    @property
+    def voronoi(self):
+        xs = [(x, y) for x in self.triangles for y in self.triangles if x != y]
+        print(f"Länge xs: {len(xs)}")
+        # print("we did it :D")
+        neighbours = []
+        # print("b4 voronoi loop")
+        for tri1, tri2 in xs:
+            # Edges are sorted neighbours
+            if (tri2, tri1) not in neighbours and set(tri1.edges).intersection(tri2.edges):
+                # print("APPEND!")
+                neighbours.append((tri1, tri2))
+        print("die schleife endet")
+        neighbours = [(x.circumcircle.center, y.circumcircle.center) for (x, y) in neighbours]
+        # print("Return")
+        return neighbours
+
 
 if __name__ == '__main__':
     n = 50
     min, max = -100, 100
+    print("unseres")
     pts = [(rd.uniform(min, max), rd.uniform(min, max)) for _ in range(n)]
     d = DelaunayTriangulation2D(radius=max + 5)  # Buffer for rounding errors
     for p in pts:
         d.add_point(p)
 
-    plt.rcParams["figure.figsize"] = (10,30)
-    figure, axis = plt.subplots(3)
+    plt.rcParams["figure.figsize"] = (20, 30)
+    figure, axis = plt.subplots(3, 2)
 
-    axis[0].set_title("meins")
-    for tri in d.triangles:
+    axis[0, 0].set_title("meins")
+    print("b4 calc trianglos")
+    trianglerinos = d.triangles
+    print("b4 print trianglos")
+    for tri in trianglerinos:
         points = np.ravel(tri.points)
-        axis[0].triplot(points[0::2], points[1::2])
+        axis[0, 0].triplot(points[0::2], points[1::2])
+    print("b4 calc voronois")
+    print(f"rawwtf: {len(d._triangles)}")
+    print(f"wtf: {len(d.triangles)}")
+    voronois = d.voronoi
+    print("b4 print voronois")
+    axis[0, 1].set_title("Voronoi-Schmoronoi sag ich immer!")
+    for (x, y) in voronois:
+        axis[0, 1].plot([x[0], y[0]], [x[1], y[1]])
 
-    axis[1].set_title("scipy")
+    print("scipy :)")
+    axis[2, 0].set_title("scipy")
     points = np.array([list(x) for x in pts])
     scipy_tri = Delaunay(pts)
-    axis[1].triplot(points[:, 0], points[:, 1], scipy_tri.simplices)
-    axis[1].plot(points[:, 0], points[:, 1], 'o')
+    axis[2, 0].triplot(points[:, 0], points[:, 1], scipy_tri.simplices)
+    axis[2, 0].plot(points[:, 0], points[:, 1], 'o')
 
-    axis[2].set_title("reference implementation")
-    d2 = Delaunay2D(radius=max+5)
+    print("ugly")
+    axis[1, 0].set_title("reference implementation")
+    d2 = Delaunay2D(radius=max + 5)
     for p in pts:
         d2.addPoint(p)
+    print(f"reftris: {d2.exportTriangles()}")
     coord, tris = d2.exportDT()
-    my_tris = [(coord[a], coord[b], coord[c]) for a,b,c in tris]
+    my_tris = [(coord[a], coord[b], coord[c]) for a, b, c in tris]
     for tri in my_tris:
         points = np.ravel(tri)
-        axis[2].triplot(points[0::2], points[1::2])
+        axis[1, 0].triplot(points[0::2], points[1::2])
+
+    axis[1, 1].set_title("ugly but working voronois >:(")
 
     plt.show()
