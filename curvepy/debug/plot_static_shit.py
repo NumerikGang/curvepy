c4f73c96df96f69a86c1ccc06b4bc7bb7d706048
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-09-19 12:31:59 +0200

    :construction: Added Comparison Plot to reference implementation

--- curvepy/debug/plot_static_shit.py
+++ curvepy/debug/plot_static_shit.py
@@ -1,13 +1,53 @@
 from curvepy.tests.test_voronoi_regions import *
-
+import numpy as np
 from curvepy.voronoi import Voronoi
 import matplotlib.pyplot as plt
+from matplotlib.tri import Triangulation
+from curvepy.debug.Reference_Implementation import Delaunay2D
 
 if __name__ == '__main__':
     for seed, mean in [*zip(SEEDS, MEANS)]:
+        # our
         d = DelaunayTriangulation2D(mean, DIAMETER)
         for s in seed:
             d.add_point(s)
         v = Voronoi(d)
-        _, axis = v.plot()
+
+        # their
+        dt = Delaunay2D(np.array(mean),100) # TODO: Maybe *50
+        for s in seed:
+            dt.addPoint(np.array(s))
+
+        plt.rcParams["figure.figsize"] = (5, 10)
+        fig, axis = plt.subplots(2)
+
+        axis[0].axis([-DIAMETER / 2 - 1, DIAMETER / 2 + 1, -DIAMETER / 2 - 1, DIAMETER / 2 + 1])
+        axis[0].set_title("meins")
+        regions = d.voronoi()
+        for p in regions:
+            polygon = [t.ccc for t in regions[p]]  # Build polygon for each region
+            axis[0].fill(*zip(*polygon), alpha=0.2)  # Plot filled polygon
+
+        # Plot voronoi diagram edges (in red)
+        for p in regions:
+            polygon = [t.ccc for t in regions[p]]  # Build polygon for each region
+            axis[0].plot(*zip(*polygon), color="red")  # Plot polygon edges in red
+
+        for tri in d.triangles:
+            x, y, z = tri.points
+            points = [*x, *y, *z]
+            axis[0].triplot(points[0::2], points[1::2], linestyle='dashed', color="blue")
+
+        # ---
+
+        axis[1].axis([-DIAMETER / 2 - 1, DIAMETER / 2 + 1, -DIAMETER / 2 - 1, DIAMETER / 2 + 1])
+        vc, vr = dt.exportVoronoiRegions()
+        cx, cy = zip(*seed)
+        dt_tris = dt.exportTriangles()
+        axis[1].triplot(Triangulation(cx, cy, dt_tris), linestyle='dashed', color='blue')
+        dt_tris = dt.exportTriangles()
+        for r in vr:
+            polygon = [vc[i] for i in vr[r]]  # Build polygon for each region
+            axis[1].plot(*zip(*polygon), color="red")  # Plot polygon edges in red
+            axis[1].fill(*zip(*polygon), alpha=0.2)
         plt.show()
