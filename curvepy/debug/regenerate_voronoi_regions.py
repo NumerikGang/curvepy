adbab432ae92f528234b9dcf6a6639794cff292b
Timo.Specht <timo.specht@stud.uni-goettingen.de>
2021-09-25 14:38:33 +0200

    :construction: WIP

--- curvepy/debug/regenerate_voronoi_regions.py
+++ curvepy/debug/regenerate_voronoi_regions.py
@@ -4,8 +4,8 @@ from curvepy.delaunay import DelaunayTriangulation2D, TriangleNode
 from curvepy.tests.test_voronoi_regions import *
 
 if __name__ == '__main__':
-    for seed, mean in [*zip(SEEDS, MEANS)]:
+    for seed, mean in [*zip([SEEDS[0]] + SEEDS[2:5] + SEEDS[7:], [MEANS[0]] + MEANS[2:5] + MEANS[7:])]:
         d = DelaunayTriangulation2D(mean, DIAMETER)
         for s in seed:
             d.add_point(s)
-        print(d.voronoi())
+        print(",",d.voronoi())
