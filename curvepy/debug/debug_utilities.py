a353da1cac34d59dc562698cb13cfd2a17386407
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-10-23 09:12:41 +0200

    :white_check_mark: Add Tests for collinear and straight line function

---
+++ curvepy/debug/debug_utilities.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+    a,b,c = x
+    print(b-a)
+    print(c-a)
+    print(np.cross(b-a,c-a))
---
+++ curvepy/tests/test_utilities.py
@@ -0,0 +1,81 @@
+from curvepy.utilities import *
+import pytest
+import numpy as np
+
+# Those are literally random generated pairs
+RANDOM_PAIRS = [(1, 3), (-2, -2), (-4, 1), (-3, -8), (2, 4), (-5, -7), (-2, -2), (-8, -6), (4, 8), (5, 1), (-2, -5),
+                (3, 1), (-2, 5), (-4, 7), (5, 4), (5, -9), (-4, 7), (4, -7), (9, 9), (-4, 5), (4, -9), (-7, 1),
+                (-9, -10), (-10, 2), (4, -6), (9, -10), (-9, -5), (2, -7), (6, -4), (-8, -1), (7, 2), (-6, 1), (2, 9),
+                (8, -8), (-2, 0), (8, -4), (-2, -7), (-6, -8), (2, -6), (0, -7), (-9, 9), (5, -8), (-3, 2), (-3, 4),
+                (8, -1), (1, -8), (-5, 0), (3, -10), (7, -1), (-1, 5)]
+
+
+@pytest.mark.parametrize('a,b', RANDOM_PAIRS)
+def test_straight_line_function_equivalent_to_value(a, b):
+    return straight_line_point(a, b, 0.5) == create_straight_line_function(a, b)(0.5)
+
+
+COLLINEAR_POINTS_X_AXIS_2D = [
+    ((1, 2), (1, 4), (1, -6)),
+    ((0, 0), (0, 0), (0, 0)),
+    ((0, 0), (0, -10), (0, 20)),
+    ((-25, 10), (-25, 10), (-25, 10)),
+    ((-1, -1000000000), (-1, 1000000000), (-1, 1337)),
+]
+
+COLLINEAR_POINTS_X_AXIS_3D = [
+    ((0, 0, 0), (0, 0, 0), (0, 0, 0)),
+    ((-25, 10, 22), (-25, 10, 22), (-25, 10, 22)),
+    ((-1, 25, -3), (-1, 50, -4), (-1, 75, -5)),
+    ((1, 230, -500), (1, 203, -500), (1, 20, -500))
+]
+
+COLLINEAR_POINTS_Y_AXIS_2D = [((a2, a1), (b2, b1), (c2, c1)) for (a1, a2), (b1, b2), (c1, c2) in
+                              COLLINEAR_POINTS_X_AXIS_2D]
+COLLINEAR_POINTS_Y_AXIS_3D = [((a2, a1, a3), (b2, b1, b3), (c2, c1, c3)) for (a1, a2, a3), (b1, b2, b3), (c1, c2, c3) in
+                              COLLINEAR_POINTS_X_AXIS_3D]
+COLLINEAR_POINTS_X_AXIS = [*COLLINEAR_POINTS_X_AXIS_2D, *COLLINEAR_POINTS_X_AXIS_3D]
+COLLINEAR_POINTS_Y_AXIS = [*COLLINEAR_POINTS_Y_AXIS_2D, *COLLINEAR_POINTS_Y_AXIS_3D]
+COLLINEAR_POINTS_Z_AXIS = [((a2, a3, a1), (b2, b3, b1), (c2, c3, c1)) for (a1, a2, a3), (b1, b2, b3), (c1, c2, c3) in
+                           COLLINEAR_POINTS_X_AXIS_3D]
+
+
+def arrayize(xss):
+    return [(np.array(a), np.array(b), np.array(c)) for a, b, c in xss]
+
+
+NOT_COLLINEAR_POINTS = [
+    ((-5, 0), (3, 5), (5, -3)),
+    ((2, 1), (3, 5), (4, -2)),
+    ((-13, 2), (3, -1), (9, -7)),
+    ((1e8, 0), (0, 1), (-1e8, 0))
+]
+
+
+@pytest.mark.parametrize('a,b,c', arrayize(COLLINEAR_POINTS_X_AXIS))
+def test_collinear_check_same_x_axis(a, b, c):
+    assert collinear_check(a, b, c)
+
+
+@pytest.mark.parametrize('a,b,c', arrayize(COLLINEAR_POINTS_Y_AXIS))
+def test_collinear_check_same_y_axis(a, b, c):
+    assert collinear_check(a, b, c)
+
+
+@pytest.mark.parametrize('a,b,c', arrayize(COLLINEAR_POINTS_Z_AXIS))
+def test_collinear_check_same_z_axis(a, b, c):
+    assert collinear_check(a, b, c)
+
+
+@pytest.mark.parametrize('a,b,c', arrayize([
+    ((1, 1), (2, 2), (3, 3)),
+    ((-2, -8), (-4, -16), (-6, -24)),
+    ((3, 0), (4, 1), (2, -1)),
+]))
+def test_collinear_not_parallel_to_axis(a, b, c):
+    assert collinear_check(a, b, c)
+
+
+@pytest.mark.parametrize('a,b,c', arrayize(NOT_COLLINEAR_POINTS))
+def test_not_collinear(a, b, c):
+    assert not collinear_check(a, b, c)
