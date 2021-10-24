51b6e5cf47809e93f9c74a537fe69da7116b6c8c
Timo.Specht <timo.specht@stud.uni-goettingen.de>
2021-10-24 19:59:09 +0200

    :hammer: minor

--- curvepy/debug/gen_bezier_tests.py
+++ curvepy/debug/gen_bezier_tests.py
@@ -29,16 +29,16 @@ def gen_test_cases(min, max, val_min, val_max):
         print(f"({difn(x, len(x) - 1)[0]},{difn(x, len(x) - 1)[0]})),")
     print("]")
 
-def gen_test_cases_for_all_forward_for_one_value(min, max, val_min, val_max):
-    print("[")
+def gen_test_cases_for_all_forward_for_one_value(min, max, val_min, val_max, fp):
+    print("[", file=fp)
     for _ in range(TEST_CASES):
         x = [random.randint(val_min, val_max) for _ in range(random.randint(min, max) + 1)]
         for i in range(len(x)):
             if random.random() < 0.5:
                 continue
-            print(f"(({x}, {i}), ")
-            print(f"{all_forward_dif(x, i)}),")
-    print("]")
+            print(f"(({x}, {i}), ", file=fp)
+            print(f"{all_forward_dif(x, i)}),", file=fp)
+    print("]", file=fp)
 
 
 gen_n = lambda n : [random.randint(-20,20) for _ in range(n+1)]
@@ -46,7 +46,9 @@ gen_n = lambda n : [random.randint(-20,20) for _ in range(n+1)]
 
 if __name__ == '__main__':
     # gen_test_cases(15, 50, -20, 20)
-    gen_test_cases_for_all_forward_for_one_value(15, 50, -20, 20)
+    with open("bla.txt", "a") as fp:
+        gen_test_cases_for_all_forward_for_one_value(15, 50, -20, 20, fp)
+
     # xs = gen_n(16)
     # ret = all_forward_dif(xs, 0)
     # for r in ret:
