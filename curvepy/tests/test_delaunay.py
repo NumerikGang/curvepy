e8bda29bdb40532f07e5f2424ef212f514f4a6dc
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-01-03 10:15:40 +0100

    :memo: docs delaunay tests

--- curvepy/tests/test_delaunay.py
+++ curvepy/tests/test_delaunay.py
@@ -1,3 +1,7 @@
+"""Here are all tests for the Delaunay triangulation algorithm.
+
+See each test for more information.
+"""
 from typing import List, Set
 
 import pytest
@@ -8,6 +12,17 @@ from curvepy.delaunay import DelaunayTriangulation2D, Point2D, TupleTriangle
 
 @pytest.mark.parametrize('xs, expected', data.RANDOMLY_UNIFORMLY_DISTRIBUTED)
 def test_random_uniform_distribution(xs: List[Point2D], expected: Set[TupleTriangle]):
+    """Checks whether the Delaunay triangulation on uniformly distributed random values works correctly.
+
+    Against precomputed correct values.
+
+    Parameters
+    ----------
+    xs: List[Point2D]
+        The points to build the triangulation from
+    expected: Set[TupleTriangle]
+        The expected triangulation
+    """
     d = DelaunayTriangulation2D(radius=10)
     for pt in xs:
         d.add_point(pt)
