866d061755f87af022e250115d0f166b2b6b7614
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-07-07 17:42:43 +0200

    :memo: __init__.py docstring

--- curvepy/__init__.py
+++ curvepy/__init__.py
@@ -1,3 +1,13 @@
 """
-Welcome to curvepy! More documentation will be written soon.
+Welcome to curvepy!
+
+curvepy is an computational geometric library. It's main focus lies on
+
+- Different Bézier curve implementations and their viability
+- A Green-Sibson/Bowyer/Watson based Delaunay triangulation, with translation in it's Voronoi dual graph
+
+The technical information can be found in the single modules, the mathematical explanations and design decisions
+can be found in the accompanying paper.
+
+Please read the paper for more information.
 """
